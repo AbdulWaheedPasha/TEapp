2ad872b4521e8a1b010e63ed83349f63a0a590b8
awaheedp <a.waheed@gmail.com>
2022-04-04 12:47:23 +0530

    update

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['touheed-education.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['touheed-education.herokuapp.com','127.0.0.1','0.0.0.0']
 
 
 # Application definition
@@ -46,9 +46,15 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 
     'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
+
+    'tinymce',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,13 +126,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE =  'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
+DATE_FORMAT = "%d %B, %Y"
+TIME_FORMAT = "%I:%M %p"
 
 
 # Static files (CSS, JavaScript, Images)
@@ -139,3 +148,79 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Mail Config
+MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.in'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'admin@touheed.education'
+EMAIL_HOST_PASSWORD = "Nekmg82YdX5a"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+
+
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
+from datetime import timedelta
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    # 'SIGNING_KEY': settings.SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/media')
+
+
+
+DJANGORESIZED_DEFAULT_SIZE = [1920, 1080]
+DJANGORESIZED_DEFAULT_QUALITY = 75
+DJANGORESIZED_DEFAULT_KEEP_META = True
+DJANGORESIZED_DEFAULT_FORCE_FORMAT = 'JPEG'
+DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'JPEG': ".jpg"}
+DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = True
--- base/urls.py
+++ base/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 from django.urls.conf import include
-from . import views
-from rest_framework import routers
-from .views import EventViewSet
 
+from rest_framework import routers
+from .views import *
 router = routers.DefaultRouter()
-router.register('event',views.EventViewSet)
+router.register('event',EventViewSet)
 
 
 urlpatterns = [
-    # path('', views.getRouter, name="routes"),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/',getUserProfile, name='users-profile'),
+    path('event-list/',getEventList, name="get-event-list"),
+    path('event-detail/<str:pk>/',get_event_detail, name="get_event_detail"),
+    path('staff-list/',get_staff_list, name="get_staff_list"),
+
+
+
+
     path('',include(router.urls)),
+    path('send-mail/', send_contact_mail, name="send_contact_mail")
+
 ]
